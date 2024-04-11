d319d8688c56c96144aa57a0d3a9cf7bfa7dcaf4
D3LETED-PL <166648001+D3LETED-PL@users.noreply.github.com>
2024-04-11 14:33:26 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Spotif","Watching YouTube"];
 
 
 let currentIndex = 0;
