d6d192d255e85637383db2e2aab2bb1a2acc3c23
D3LETED-PL <166648001+D3LETED-PL@users.noreply.github.com>
2024-05-15 20:41:34 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["By D3LETED 🖊️","By D3LETED 🖊️"];
+const statusMessages = ["By D3LETED 🖊️","Ogląda EditVerse 🎥"];
 
 
 let currentIndex = 0;
